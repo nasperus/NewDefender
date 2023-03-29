6b4eb4d7a13fada75a0f807266ea94c2f0d0f032
Giorgi <gio.melikidze095@gmail.com>
2023-03-30 00:48:53 +0400

    update

--- Assets/Scripts/PowerUp.cs
+++ Assets/Scripts/PowerUp.cs
@@ -4,17 +4,48 @@ using UnityEngine;
 
 public class PowerUp : MonoBehaviour
 {
+    
     [SerializeField] GameObject[] powerUp;
     public static PowerUp instance;
+    private float padding = 0.5f;
+    private int xMax = 17, xMin = 17;
+    private int speed = 10;
+    Vector2 newPos;
+
     private void Awake()
     {
         instance = this;
+
+        
+       
     }
-    void Start()
+
+
+    private void Start()
     {
-        Instantiate(powerUp[0], transform.position, transform.rotation);
+     
+        StartCoroutine(PowerDown());
     }
 
 
+    private void PowerUpScore()
+    {
+
+        var random = Random.Range(-xMin, xMax);
+        newPos = new Vector2(random, transform.position.y);  
+        var spawn = Instantiate(powerUp[0], newPos, transform.rotation);
+        spawn.GetComponent<Rigidbody2D>().velocity = Vector2.down * speed;
+
+    }
+
+    IEnumerator PowerDown()
+    {
+        yield return new WaitForSeconds(10f);
+        while (true)
+        {
+            PowerUpScore();
+            yield return new WaitForSeconds(10f);
+        }
+    }
 
 }
